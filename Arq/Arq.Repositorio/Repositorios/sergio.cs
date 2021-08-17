25cc4f28783c48a9d5da3b7ea462b3a4f9c0a66a
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2021-08-17 13:15:17 -0300

    Sergio segun sergio

--- Arq/Arq.Repositorio/Repositorios/sergio.cs
+++ Arq/Arq.Repositorio/Repositorios/sergio.cs
@@ -8,5 +8,19 @@ namespace Arq.Repositorio.Repositorios
 {
     public class sergio
     {
+        private int myVar;
+        private readonly int arg;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        public sergio(int arg)
+        {
+            this.arg = arg;
+        }
+
     }
 }
